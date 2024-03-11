150f25cb75c46f029239d46ca4a226213c5ebebc
kriti8kum <157258583+kriti8kum@users.noreply.github.com>
2024-03-11 18:22:59 +0530

    Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly(); // Create an object of RWOnly
+
+        // Try setting a value directly (will result in an error)
+        // obj.name = "John"; // Uncommenting this line will cause an error
+
+        // Set the value using the setter method
+        obj.setName("John");
+
+        // Access the value using the getter method
+        String retrievedName = obj.getName();
+        System.out.println("Name retrieved from RWOnly object: " + retrievedName);
+    }
 }
