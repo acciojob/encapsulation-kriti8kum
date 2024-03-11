6942047da44245185ce90cb256077a730f19854f
kriti8kum <157258583+kriti8kum@users.noreply.github.com>
2024-03-11 18:22:07 +0530

    RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name; // Private member variable
+
+    // Getter method to access the name
+    public String getName() {
+        return name;
+    }
+
+    // Setter method to set the name
+    public void setName(String newName) {
+        name = newName;
+    }
 }
+
